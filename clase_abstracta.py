32aa3d768c6f3a3de6da5a2c8df6ad91a84c31dd
Luis Tepetate <luistepetate1@gmail.com>
2025-03-23 20:18:08 -0600

    this commit correct errors on rama-core

---
+++ clase_abstracta.py
@@ -0,0 +1,36 @@
+#Fichero de la clase abstracta para las tablas
+from abc import ABC, abstractmethod
+
+class BaseDatos(ABC):
+    @abstractmethod
+    def agregar_dato(self, dato):
+        """
+        Agrega un nuevo registro a la base de datos.
+        :param dato: El dato a agregar. Debe ser un objeto con los atributos necesarios.
+        """
+        pass
+
+    @abstractmethod
+    def modificar_dato(self, dato):
+        """
+        Modifica un registro existente en la base de datos.
+        :param dato: El dato a modificar. Debe ser un objeto con los atributos necesarios.
+        """
+        pass
+
+    @abstractmethod
+    def eliminar_dato(self, dato):
+        """
+        Elimina un registro de la base de datos.
+        :param dato: El dato a eliminar. Debe ser un objeto con los atributos necesarios.
+        """
+        pass
+    def mostrar_datos(self):
+        """
+        Muestra todos los registros de la base de datos.
+        """
+        # Lógica para mostrar los datos
+        pass
+
+    def agregar_muchos_datos(self,lista):
+        pass
